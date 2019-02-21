e6c4d40f0eaa6c93cac88582d862aa8393a3cc10
Martin Vilcans <martin@librador.com>
2019-02-21 21:53:08 +0100

    Add install_requires six for universal Python support

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setup(
     author='Martin Vilcans',
     author_email='screenplain@librador.com',
     url='http://www.screenplain.com/',
+    install_requires=[
+        'six',
+    ],
     extras_require={
         'PDF': 'reportlab'
     },
