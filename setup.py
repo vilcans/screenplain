535d8d69b8844cfdc1b3523737be69a78b7e3954
Martin Vilcans <martin@librador.com>
2019-02-21 21:59:54 +0100

    Bump version and copyright

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='screenplain',
-    version='0.7.0',
+    version='0.8.0',
     description='Convert text file to viewable screenplay.',
     author='Martin Vilcans',
     author_email='screenplain@librador.com',
