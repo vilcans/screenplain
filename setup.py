9d972bfa792c08d4f3ce6a6b7cfd9877f801b5e3
Martin Vilcans <martin@librador.com>
2020-07-12 01:09:47 +0200

    Use content or README.md as long_description

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@
 
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='screenplain',
     version='0.9.0',
     description='Convert text file to viewable screenplay.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Martin Vilcans',
     author_email='screenplain@librador.com',
     url='http://www.screenplain.com/',
