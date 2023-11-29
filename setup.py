41ac809564dc3a885baacbb5cd5cb513e4caac78
Martin Vilcans <martin@librador.com>
2023-11-29 08:33:02 +0100

    Bump version to 0.11.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='screenplain',
-    version='0.10.0',
+    version='0.11.0',
     description='Convert text file to viewable screenplay.',
     long_description=long_description,
     long_description_content_type='text/markdown',
